402594222a2dd592d7c7c584c1c724692b226a60
Nathaniel Pyon <nathaniel.pyon@mrjaffesclass.com>
2022-09-23 11:45:26 -0700

    a

---
+++ If Statements/IfStatements.java
@@ -0,0 +1,34 @@
+public class IfStatements
+{
+    int a = 5;
+    int b = 2;
+    int c = 1;
+    int d = -8;
+
+    public void run() {
+        System.out.println("Hello World!");
+        if (a == 5) 
+            System.out.println("SUCCESS");
+            else
+                System.out.println("FAILURE");
+        if (b == 2)
+            System.out.println("SUCCESS");
+            else
+                System.out.println("FAILURE");
+        if (c != 1)
+            System.out.println("SUCCESS");
+            else
+                System.out.println("FAILURE");
+        if (d == -8 && a == 4)
+            System.out.println("SUCCESS");
+            else 
+                System.ou.println("FAILURE");
+    }
+    
+    
+    public static void main(String[] args)
+    {        
+        IfStatements hw = new IfStatements();
+        hw.run();
+    }
+}
